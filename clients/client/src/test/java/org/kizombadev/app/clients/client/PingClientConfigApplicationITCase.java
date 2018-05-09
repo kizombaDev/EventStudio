ab462046ef30c2c0f5c8c547ff00679ff28f5e3c
Marcel Swoboda <marcel.swoboda@gmx.de>
2018-05-09 19:17:11 +0200

    add integration test phase

--- clients/client/src/test/java/org/kizombadev/app/clients/client/PingClientConfigApplicationITCase.java
+++ clients/client/src/test/java/org/kizombadev/app/clients/client/PingClientConfigApplicationITCase.java
@@ -1,5 +1,6 @@
 package org.kizombadev.app.clients.client;
 
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,9 +11,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PingClientConfigApplicationTests {
+public class PingClientConfigApplicationITCase {
 
     @Test
     public void contextLoads() {
+        Assert.fail();
     }
 }
