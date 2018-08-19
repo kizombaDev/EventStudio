a10bb2ffe7e3d3f7dfd73f534683bdc31442a422
Marcel Swoboda <marcel.swoboda@gmx.de>
2018-08-19 23:11:42 +0200

    improve code coverage

---
+++ event-pipeline/src/test/java/org/kizombadev/eventstudio/eventpipeline/output/ElasticsearchOutputITCase.java
@@ -0,0 +1,66 @@
+package org.kizombadev.eventstudio.eventpipeline.output;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kizombadev.eventstudio.common.EventKeys;
+import org.kizombadev.eventstudio.common.elasticsearch.ElasticsearchService;
+import org.kizombadev.eventstudio.common.elasticsearch.FilterCriteriaDto;
+import org.kizombadev.eventstudio.common.elasticsearch.FilterOperation;
+import org.kizombadev.eventstudio.common.elasticsearch.FilterType;
+import org.kizombadev.eventstudio.eventpipeline.EventEntry;
+import org.kizombadev.eventstudio.eventpipeline.properties.FilterProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestConfiguration
+public class ElasticsearchOutputITCase {
+
+    @Autowired
+    private ElasticsearchOutput elasticsearchOutput;
+
+    @Autowired
+    private ElasticsearchService elasticsearchService;
+
+    @Test
+    public void test() {
+        //arrange
+        Map<String, Object> source = new HashMap<String, Object>() {{
+            put(EventKeys.SOURCE_ID, "foo");
+            put(EventKeys.TYPE, "ping");
+            put(EventKeys.TIMESTAMP, LocalDateTime.of(2014,1,1,1,1).toString());
+            put(EventKeys.BYTES, "32");
+        }};
+        EventEntry eventEntry = new EventEntry(source);
+
+        //act
+        elasticsearchOutput.write(Collections.singletonList(eventEntry));
+
+        //assert
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        FilterCriteriaDto dto = new FilterCriteriaDto();
+        dto.setField(EventKeys.SOURCE_ID);
+        dto.setOperator(FilterOperation.EQUALS);
+        dto.setValue("foo");
+        dto.setType(FilterType.PRIMARY);
+        List<Map<String, Object>> result = elasticsearchService.getElementsByFilter(Collections.singletonList(dto), 0, 1);
+        Assert.assertEquals(1, result.size());
+        Map<String, Object> data = result.get(0);
+        Assert.assertEquals("foo", data.get(EventKeys.SOURCE_ID) );
+        Assert.assertEquals("ping", data.get(EventKeys.TYPE) );
+    }
+}
