d6d84b36945e5440c119a6ecad97ffb0905c4237
Marcel Swoboda <marcel.swoboda@gmx.de>
2018-08-19 23:21:36 +0200

    improve code coverage

---
+++ event-pipeline/src/test/java/org/kizombadev/eventstudio/eventpipeline/output/ConsoleOutputTest.java
@@ -0,0 +1,50 @@
+package org.kizombadev.eventstudio.eventpipeline.output;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.kizombadev.eventstudio.common.EventKeys;
+import org.kizombadev.eventstudio.eventpipeline.EventEntry;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class ConsoleOutputTest {
+
+    private ConsoleOutput underTest;
+    private  EventEntry eventEntry;
+
+    @Before
+    public void init() {
+        underTest = new ConsoleOutput();
+
+        Map<String, Object> source = new HashMap<String, Object>() {{
+            put(EventKeys.SOURCE_ID, "abc");
+            put(EventKeys.TYPE, "access_log");
+            put(EventKeys.TIMESTAMP, LocalDateTime.of(2014,1,1,1,1).toString());
+        }};
+         eventEntry = new EventEntry(source);
+    }
+
+    @Test
+    public void write() {
+        //act
+        underTest.write(eventEntry);
+
+        //Assert
+        //no exception is thrown
+    }
+
+    @Test
+    public void writeAsCollection() {
+        //act
+        underTest.write(Collections.singletonList(eventEntry));
+
+        //Assert
+        //no exception is thrown
+    }
+}
