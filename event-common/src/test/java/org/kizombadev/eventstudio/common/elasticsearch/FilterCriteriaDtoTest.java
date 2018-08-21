01ce987211abf287cf386d1aeb29cc3d6213a134
Marcel Swoboda <marcel.swoboda@gmx.de>
2018-08-21 17:46:27 +0200

    improve code coverage

---
+++ event-common/src/test/java/org/kizombadev/eventstudio/common/elasticsearch/FilterCriteriaDtoTest.java
@@ -0,0 +1,36 @@
+package org.kizombadev.eventstudio.common.elasticsearch;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.kizombadev.eventstudio.common.EventKeys;
+
+import static org.junit.Assert.*;
+
+public class FilterCriteriaDtoTest {
+
+    private final    FilterCriteriaDto dto = new FilterCriteriaDto(EventKeys.TYPE, "ping", FilterType.PRIMARY, FilterOperation.EQUALS);
+
+
+    @Test
+    public void testToString() {
+        Assert.assertEquals("FilterCriteriaDto{field=type, value=ping, type=primary, operator=equals}", dto.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        FilterCriteriaDto one = new FilterCriteriaDto(EventKeys.TYPE, "ping", FilterType.PRIMARY, FilterOperation.EQUALS);
+        FilterCriteriaDto two = new FilterCriteriaDto(EventKeys.TYPE, "ping", FilterType.PRIMARY, FilterOperation.EQUALS);
+
+        //act & assert
+        Assert.assertEquals(one, two);
+    }
+
+    @Test
+    public void testHashCode() {
+        FilterCriteriaDto one = new FilterCriteriaDto(EventKeys.TYPE, "ping", FilterType.PRIMARY, FilterOperation.EQUALS);
+        FilterCriteriaDto two = new FilterCriteriaDto(EventKeys.TYPE, "ping", FilterType.PRIMARY, FilterOperation.EQUALS);
+
+        //act & assert
+        Assert.assertEquals(one.hashCode(), two.hashCode());
+    }
+}
