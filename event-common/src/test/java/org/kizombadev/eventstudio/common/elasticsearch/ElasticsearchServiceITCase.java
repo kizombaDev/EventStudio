37beb6ea27e78fbf2c7a13350b5979dbf22ae2cc
Marcel Swoboda <marcel.swoboda@gmx.de>
2018-08-21 16:59:58 +0200

    fix integration test

--- event-common/src/test/java/org/kizombadev/eventstudio/common/elasticsearch/ElasticsearchServiceITCase.java
+++ event-common/src/test/java/org/kizombadev/eventstudio/common/elasticsearch/ElasticsearchServiceITCase.java
@@ -20,7 +20,7 @@ import java.util.*;
 @TestConfiguration
 public class ElasticsearchServiceITCase {
 
-    private static final int TIMEOUT = 1;
+    private static final int TIMEOUT = 5;
 
     @Autowired
     private ElasticsearchService elasticSearchService;
@@ -33,20 +33,26 @@ public class ElasticsearchServiceITCase {
         elasticSearchService.prepareMappingField(EventKeys.TYPE, FieldTypes.KEYWORD_TYPE);
         elasticSearchService.prepareMappingField(EventKeys.SEQUENTIAL_TIME_ID, FieldTypes.INTEGER_TYPE);
         elasticSearchService.deleteEvents(0);
-
         sleep(TIMEOUT);
+
         final Map<String, Object> pingOne = new HashMap<>();
-        pingOne.put(EventKeys.TIMESTAMP, LocalDateTime.of(2014,8,14,12,12,12).toString());
+        pingOne.put(EventKeys.TIMESTAMP, LocalDateTime.of(2014, 8, 14, 12, 12, 12).toString());
         pingOne.put(EventKeys.SOURCE_ID, "fau_ping");
         pingOne.put(EventKeys.TYPE, "ping");
         pingOne.put(EventKeys.SEQUENTIAL_TIME_ID, 1);
+
         final Map<String, Object> pingTwo = new HashMap<>();
-        pingTwo.put(EventKeys.TIMESTAMP, LocalDateTime.of(2014,8,15,12,12,12).toString());
+        pingTwo.put(EventKeys.TIMESTAMP, LocalDateTime.of(2014, 8, 15, 12, 12, 12).toString());
         pingTwo.put(EventKeys.SOURCE_ID, "facebook_ping");
         pingTwo.put(EventKeys.TYPE, "ping");
         pingTwo.put(EventKeys.SEQUENTIAL_TIME_ID, 2);
 
-        elasticSearchService.bulkInsert(Arrays.asList(pingOne, pingTwo));
+        final Map<String, Object> accessLog = new HashMap<>();
+        accessLog.put(EventKeys.TIMESTAMP, LocalDateTime.of(2014, 9, 15, 12, 12, 12).toString());
+        accessLog.put(EventKeys.SOURCE_ID, "fcs");
+        accessLog.put(EventKeys.TYPE, "access_log");
+
+        elasticSearchService.bulkInsert(Arrays.asList(pingOne, pingTwo, accessLog));
         sleep(TIMEOUT);
     }
 
@@ -66,8 +72,8 @@ public class ElasticsearchServiceITCase {
         //assert
         Assert.assertEquals(1, result.size());
         Map<String, Object> data = result.get(0);
-        Assert.assertEquals("facebook_ping", data.get(EventKeys.SOURCE_ID) );
-        Assert.assertEquals("ping", data.get(EventKeys.TYPE) );
+        Assert.assertEquals("fcs", data.get(EventKeys.SOURCE_ID));
+        Assert.assertEquals("access_log", data.get(EventKeys.TYPE));
     }
 
     @Test
@@ -81,8 +87,8 @@ public class ElasticsearchServiceITCase {
         //assert
         Assert.assertEquals(1, result.size());
         Map<String, Object> data = result.get(0);
-        Assert.assertEquals("fau_ping", data.get(EventKeys.SOURCE_ID) );
-        Assert.assertEquals("ping", data.get(EventKeys.TYPE) );
+        Assert.assertEquals("fau_ping", data.get(EventKeys.SOURCE_ID));
+        Assert.assertEquals("ping", data.get(EventKeys.TYPE));
     }
 
     @Test
@@ -117,7 +123,29 @@ public class ElasticsearchServiceITCase {
         //act
         elasticSearchService.updateField(Collections.singletonList(dto), EventKeys.SEQUENTIAL_TIME_ID, "42");
 
+        //assert
         sleep(TIMEOUT);
         Assert.assertEquals(42, elasticSearchService.getMaxValue(Collections.singletonList(dto), EventKeys.SEQUENTIAL_TIME_ID), 1.000);
+    }
+
+    @Test
+    public void testGetDateHistogram() {
+        //arrange
+        FilterCriteriaDto dto = new FilterCriteriaDto(EventKeys.TYPE, "ping", FilterType.PRIMARY, FilterOperation.EQUALS);
+        final String primaryCount = "primary_count";
+        final String secondaryCount = "secondary_count";
+        final String key = "key";
+
+        //act
+        List<Map<String, Object>> dateHistogram = elasticSearchService.getDateHistogram(Collections.singletonList(dto));
+
+        //assert
+        Assert.assertEquals(2, dateHistogram.size());
+        Assert.assertEquals("14-08-2014", dateHistogram.get(0).get(key));
+        Assert.assertEquals(1L, dateHistogram.get(0).get(primaryCount));
+        Assert.assertEquals(1L, dateHistogram.get(0).get(secondaryCount));
+        Assert.assertEquals("15-08-2014", dateHistogram.get(1).get(key));
+        Assert.assertEquals(1L, dateHistogram.get(1).get(primaryCount));
+        Assert.assertEquals(1L, dateHistogram.get(1).get(secondaryCount));
     }
 }
