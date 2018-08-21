147df64f7f203154653f4cb2c4055488815bd158
Marcel Swoboda <marcel.swoboda@gmx.de>
2018-08-21 16:46:51 +0200

    fix integration test

--- event-common/src/test/java/org/kizombadev/eventstudio/common/elasticsearch/ElasticsearchServiceITCase.java
+++ event-common/src/test/java/org/kizombadev/eventstudio/common/elasticsearch/ElasticsearchServiceITCase.java
@@ -20,6 +20,8 @@ import java.util.*;
 @TestConfiguration
 public class ElasticsearchServiceITCase {
 
+    private static final int TIMEOUT = 1;
+
     @Autowired
     private ElasticsearchService elasticSearchService;
 
@@ -32,7 +34,7 @@ public class ElasticsearchServiceITCase {
         elasticSearchService.prepareMappingField(EventKeys.SEQUENTIAL_TIME_ID, FieldTypes.INTEGER_TYPE);
         elasticSearchService.deleteEvents(0);
 
-        sleep(5);
+        sleep(TIMEOUT);
         final Map<String, Object> pingOne = new HashMap<>();
         pingOne.put(EventKeys.TIMESTAMP, LocalDateTime.of(2014,8,14,12,12,12).toString());
         pingOne.put(EventKeys.SOURCE_ID, "fau_ping");
@@ -45,7 +47,7 @@ public class ElasticsearchServiceITCase {
         pingTwo.put(EventKeys.SEQUENTIAL_TIME_ID, 2);
 
         elasticSearchService.bulkInsert(Arrays.asList(pingOne, pingTwo));
-        sleep(5);
+        sleep(TIMEOUT);
     }
 
     private void sleep(int seconds) {
@@ -56,11 +58,12 @@ public class ElasticsearchServiceITCase {
         }
     }
 
-    //TODO write more tests
-
     @Test
     public void testGetElementsByFilter() {
+        //act
         List<Map<String, Object>> result = elasticSearchService.getElementsByFilter(new ArrayList<>(), 0, 1);
+
+        //assert
         Assert.assertEquals(1, result.size());
         Map<String, Object> data = result.get(0);
         Assert.assertEquals("facebook_ping", data.get(EventKeys.SOURCE_ID) );
@@ -69,9 +72,13 @@ public class ElasticsearchServiceITCase {
 
     @Test
     public void testGetElementsByFilterWithCriteriaFilter() {
+        //arrange
         FilterCriteriaDto dto = new FilterCriteriaDto(EventKeys.SOURCE_ID, "fau_ping", FilterType.PRIMARY, FilterOperation.EQUALS);
 
+        //act
         List<Map<String, Object>> result = elasticSearchService.getElementsByFilter(Collections.singletonList(dto), 0, 1);
+
+        //assert
         Assert.assertEquals(1, result.size());
         Map<String, Object> data = result.get(0);
         Assert.assertEquals("fau_ping", data.get(EventKeys.SOURCE_ID) );
@@ -80,11 +87,14 @@ public class ElasticsearchServiceITCase {
 
     @Test
     public void testGetFieldStructure() {
+        //act
         List<Map<String, String>> fieldStructure = elasticSearchService.getFieldStructure();
-        Assert.assertEquals(3, fieldStructure.size());
+
+        //assert
+        Assert.assertEquals(4, fieldStructure.size());
         Map<String, String> test = fieldStructure.get(0);
-        Assert.assertEquals(EventKeys.SOURCE_ID, test.get("field"));
-        Assert.assertEquals(FieldTypes.KEYWORD_TYPE, test.get("type"));
+        Assert.assertEquals(EventKeys.SEQUENTIAL_TIME_ID, test.get("field"));
+        Assert.assertEquals(FieldTypes.INTEGER_TYPE, test.get("type"));
     }
 
     @Test
@@ -97,5 +107,17 @@ public class ElasticsearchServiceITCase {
 
         //assert
         Assert.assertEquals(1.0, maxValue, 0.00001);
+    }
+
+    @Test
+    public void testUpdateField() {
+        //arrange
+        FilterCriteriaDto dto = new FilterCriteriaDto(EventKeys.SOURCE_ID, "fau_ping", FilterType.PRIMARY, FilterOperation.EQUALS);
+
+        //act
+        elasticSearchService.updateField(Collections.singletonList(dto), EventKeys.SEQUENTIAL_TIME_ID, "42");
+
+        sleep(TIMEOUT);
+        Assert.assertEquals(42, elasticSearchService.getMaxValue(Collections.singletonList(dto), EventKeys.SEQUENTIAL_TIME_ID), 1.000);
     }
 }
