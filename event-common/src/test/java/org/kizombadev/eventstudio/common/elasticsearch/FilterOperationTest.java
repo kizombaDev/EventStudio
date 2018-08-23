a93583538062e44493a120bb2fc3d765e72fcc5f
Marcel Swoboda <marcel.swoboda@gmx.de>
2018-08-23 20:30:13 +0200

    code coverage

---
+++ event-common/src/test/java/org/kizombadev/eventstudio/common/elasticsearch/FilterOperationTest.java
@@ -0,0 +1,27 @@
+package org.kizombadev.eventstudio.common.elasticsearch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FilterOperationTest {
+
+    @Test
+    public void testForValue() {
+        Assert.assertEquals(FilterOperation.EQUALS, FilterOperation.forValue("equals"));
+    }
+
+    @Test(expected =  IllegalStateException.class)
+    public void testForValueWithInvalidType() {
+        FilterOperation.forValue("foo");
+    }
+
+    @Test
+    public void testToString() {
+        Assert.assertEquals("equals", FilterOperation.EQUALS.toString());
+    }
+
+    @Test
+    public void testGetValue() {
+        Assert.assertEquals("equals", FilterOperation.EQUALS.getValue());
+    }
+}
